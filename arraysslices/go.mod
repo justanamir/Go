d11235cbe495960f9f262e7c8afbd5a37cf67c1c
justanamir <amirdanialmuslim@gmail.com>
2024-08-07 20:38:16 +0800

    update

---
+++ arraysslices/go.mod
@@ -0,0 +1,3 @@
+module arraysslices
+
+go 1.22.3
---
+++ pointerserrors/wallet.go
@@ -0,0 +1,42 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) BalanceX10() {
+	w.balance = w.balance * 10
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil
+}
