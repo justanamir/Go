e112a064bbc0770432924f760ea892dfd61bdc7f
justanamir <amirdanialmuslim@gmail.com>
2025-01-02 03:19:51 +0000

    snippetbox 7.6

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 
 	"snippetbox.justanamir.net/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -17,6 +18,7 @@ type application struct {
 	logger *slog.Logger
 	snippets *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder *form.Decoder
 }
 
 func main() {
@@ -41,11 +43,14 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+
+	formDecoder := form.NewDecoder()
 	
 	app := &application{
 		logger: logger,
 		snippets: &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder: formDecoder,
 	}
 
 	logger.Info("starting server","addr",*addr)
