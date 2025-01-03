f341eff6089a06f1d18c2105d37ccc271532c880
justanamir <amirdanialmuslim@gmail.com>
2025-01-03 02:31:54 +0000

    snippetbox 10.1

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -73,6 +73,9 @@ func main() {
 		Handler:  app.routes(),
 		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 		TLSConfig: tlsConfig,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", *addr)
